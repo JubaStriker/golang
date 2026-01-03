a72cce02ed8bd18442411c8550582d181bf2a818
JubaStriker <jubair@transfi.com>
2026-01-03 23:25:54 +0600

    More variabled

--- 01hello/main.go
+++ 01hello/main.go
@@ -5,5 +5,13 @@ import "fmt"
 func main() {
 	var username string = "Jubair"
 	fmt.Println(username)
-	fmt.Printf("The value of the variable is %T \n", username)
+	fmt.Printf("The type of the variable of type %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("The type of isLoggedIn variable is %T \n", isLoggedIn)
+
+	var smallValue uint8 = 255
+	fmt.Println(smallValue)
+
 }
